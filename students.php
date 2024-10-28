7d1ea09a41d44ec6b9053efd98d1bc4a9d43547f
liuuu20 <asdsa654556@gmail.com>
2024-10-28 21:18:18 +0800

    更新dashboard.php
    更新dashboard.php

--- students.php
+++ students.php
@@ -1,28 +1,15 @@
-<<<<<<< HEAD
  <?php
-=======
-<?php
->>>>>>> 55a6a09ecd2869ee718498523301cc656795bd53
-// 資料庫連線設定
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "113dbb06";
-
-// 建立資料庫連線
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// 檢查連線是否成功
-if ($conn->connect_error) {
-<<<<<<< HEAD
-    die("資料庫連線失敗: " . $conn->connect_error);
+if (mysqli_connect_errno()) {
+    echo "连接至 MySQL 失败: " . mysqli_connect_error();
 }
+
+$conn = mysqli_connect('localhost', '113dbb06', '2476-3247', '113dbb06');
+mysqli_query($conn, 'SET NAMES utf8');
+mysqli_query($conn, 'SET CHARACTER_SET_CLIENT=utf8');
+mysqli_query($conn, 'SET CHARACTER_SET_RESULTS=utf8');
 
 // 設置資料庫連線的字符集為 UTF-8
 $conn->set_charset("utf8mb4");
-
-
-
 
 ?>
 
@@ -94,7 +81,7 @@ $conn->set_charset("utf8mb4");
           <ul>
             <li><a href="index.html" class="active">首頁</a></li>
             <li><a href="news.html">最新消息</a></li>
-            <li><a href="students.html">學生查詢</a></li>
+            <li><a href="students.php">學生查詢</a></li>
             <li><a href="lesson.html">課程查詢</a></li>
             <li><a href="personal.html">個人設置</a></li>
             <li><a href="contact.html">聯絡我們</a></li>
@@ -207,34 +194,3 @@ if (isset($_GET['student_field']) && isset($_GET['student_key'])) {
     <script src="assets/js/main.js"></script>
   </body>
 </html>
-=======
-    die("連線失敗: " . $conn->connect_error);
-}
-echo "連線成功<br>";
-echo "目前連接的資料庫: " . $dbname . "<br>";
-
-// 設置資料庫連線的字符集為 UTF-8
-$conn->set_charset("utf8mb4");
-
-// 查詢 student 資料表中的所有資料
-$sql = "SELECT * FROM student";
-$result = $conn->query($sql);
-
-// 檢查是否查詢失敗
-if ($result === false) {
-    die("查詢失敗: " . $conn->error);
-}
-
-// 檢查查詢結果並輸出資料
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        echo "學號: " . $row["studentid"] . " - 姓名: " . $row["name"] . "<br>";
-    }
-} else {
-    echo "沒有找到資料";
-}
-
-// 關閉資料庫連線
-$conn->close();
-?>
->>>>>>> 55a6a09ecd2869ee718498523301cc656795bd53
